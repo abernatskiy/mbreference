0e0125e50783f2dfd4d6293b30c8812384c73d7f
Anton Bernatskiy (agave) <abernats@uvm.edu>
2019-07-15 16:47:25 -0700

    Macros values for MABE comparison
    	modified:   constants.h

--- constants.h
+++ constants.h
@@ -1,8 +1,8 @@
 #pragma once
 
 // mb/gate.h definitions
-#define MB_GATES_NUM_INPUTS {1, 2}
-#define MB_GATES_NUM_OUTPUTS {1, 2}
+#define MB_GATES_NUM_INPUTS {1, 4}
+#define MB_GATES_NUM_OUTPUTS {1, 4}
 
 // mb/mb.h|.cpp definitions
 #define MB_GATE_INSERTION_PROB 0.1
@@ -14,17 +14,17 @@
 #define INDIVIDUAL_MB_VETO_BITS 1
 #define INDIVIDUAL_MB_TIME_STEPS 30
 #define INDIVIDUAL_MB_HIDDEN_NODES 10
-#define INDIVIDUAL_MB_INITIAL_GATES 2
+#define INDIVIDUAL_MB_INITIAL_GATES 10
 
 // evolution/* definitions
 #define EVALUATIONS_TO_OPTIMIZE {"negScore", "mbg"}
 
-#define EVOLVER_MAX_GENERATIONS 200 // -1 for indefinite
-#define EVOLVER_POPULATION_SIZE 100
+#define EVOLVER_MAX_GENERATIONS 2000 // -1 for indefinite
+#define EVOLVER_POPULATION_SIZE 400
 
 #define EVOLVER_EVALUATIONS_OF_INTEREST {"negScore", "mbg"}
 
-#define EVOLVER_RECORDS_PERIOD 100 // -1 for never
+#define EVOLVER_RECORDS_PERIOD 1000 // -1 for never
 #define EVOLVER_EVALUATIONS_OF_INTEREST_PERIOD 1 // -1 for never
 #define EVOLVER_EVALUATIONS_OF_INTEREST_INDEPENDENT false
 
@@ -32,7 +32,7 @@
 
 #define BREAK_TIES_BY_ID true // the default is true
 
-#define NUM_THREADS 8
+#define NUM_THREADS 10
 
 // common macros
 #define DEBUG
